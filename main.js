710553bf3be8662c759262a102f697cb0592ea2c
Christopher <voschristopher53@gmail.com>
2024-03-21 19:58:30 +0200

    removed IIFE function

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ const nodeCron = require('node-cron')
 
 
 // Main function programs starting point
-(function main() {
+function main() {
   
 
   console.log("EOP 69"); 
   console.log("sixty nine");
   
-})()
+}
+
+main()
