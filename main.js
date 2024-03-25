dca63ec72ba8596a403e72756feea0963487188a
christopher <christophervos@gmail.com>
2024-03-25 15:25:09 +0200

    added intents & reformatted code

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 //Import required libraries
 require("dotenv").config()// initialize environment variables 
-const discordLibrary = require('discord.js')
+const { Client } = require('discord.js')
 const nodeCron = require('node-cron')
 // gets token from the .env file
 const DISCORD_TOKEN = process.env.TOKEN
-
-// add a new discord client instance
-const client = new Discord.client();
-//Set up Bot's login
-client.login(DISCORD_TOKEN);
-
+// Channel ID
+const CHANNEL_ID = process.env.CHANNEL_ID
 // the starting hour message its in the 24 hour format
 const START_HOUR = process.env.START_HOUR
 // the ending hour message its in the 30 hour format
 const END_HOUR = process.env.END_HOUR
 
+
 // Main function programs starting point
-function main() {
-  console.log('starting hour is ' + START_HOUR);
-  console.log('ending hour is '+ END_HOUR);
-  const channel = client.channels.cache.get('Channel ID goes here!');
+async function main() {
+  // add a new discord client instance
+  const client = new Client({ intents: ["GuildMessages", "Guilds"] });
+  //Set up Bot's login
+  await client.login(DISCORD_TOKEN);
+
+  const channel = client.channels.cache.get(CHANNEL_ID);
 
   // Schedule task to run from Monday to Friday
   nodeCron.schedule(`0 ${START_HOUR} * * 1-5`, () => {
@@ -42,5 +42,5 @@ function main() {
   console.log("sixty nine");
 }
 
-main()
+main().catch(err=> console.log(err))
 
